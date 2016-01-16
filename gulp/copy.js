f559f7c30604949130d47b12a511e906e0b179df
Ivan Mironchik <ivan@MacBook-Air-Ivan.local>
2016-01-16 10:35:55 +0300

    upd

--- gulp/copy.js
+++ gulp/copy.js
@@ -4,8 +4,16 @@
   var gulp = require('gulp');
 
   gulp.task('copy', function() {
-    gulp.src(['source/images/*.png', 'source/images/**/*.png', '!source/images/sprite/*', '!source/images/sprite/'])
+    gulp.src([
+        'source/images/*.png',
+        'source/images/**/*.png',
+        'source/images/*.jpg',
+        'source/images/**/*.jpg',
+        '!source/images/sprite/*',
+        '!source/images/sprite/'
+    ])
       .pipe(gulp.dest('build/img'));
+
     gulp.src('source/fonts/*.woff')
       .pipe(gulp.dest('build/fonts'));
   });
